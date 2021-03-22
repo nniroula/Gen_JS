4bdc2560d0a8078ae4d96a55e8b5fd29dbfbf815
Nabin <nkbhaie2017@gmail.com>
2021-03-21 22:13:11 -0600

    first htmel and js

---
+++ practice.js
@@ -0,0 +1,8 @@
+let newMessage = "Hello friends let's learn coding a website";
+function updateThisInWebPage(){
+    let element = document.getElementById("firstPractice");
+    element.innerText = newMessage;
+}
+updateThisInWebPage();
+
+// this will replace the content "Pleae Learn to Code Fast!" preetn in html.
